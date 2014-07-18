10bf2c18675298e881ec3082bac2d15c89dbe1d0
Brad Egler <begler@gmail.com>
2014-07-18 07:19:57 -0400

    moved java to java7

--- buildall.sh
+++ buildall.sh
@@ -2,7 +2,7 @@
 
 docker build -t="bradegler/base" ./base
 docker build -t="bradegler/chef" ./chef
-docker build -t="bradegler/java" ./java
+docker build -t="bradegler/java7" ./java7
 docker build -t="bradegler/jenkins" ./jenkins
 docker build -t="bradegler/rabbitmq" ./rabbitmq
 docker build -t="bradegler/mongodb" ./mongodb
