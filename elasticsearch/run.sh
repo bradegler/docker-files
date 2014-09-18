0739314e5a54d320e31902b43b7f66493d58d064
Brad Egler <begler@gmail.com>
2014-09-18 13:53:20 -0400

    updated run script for elasticsearch

--- elasticsearch/run.sh
+++ elasticsearch/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p /tmp/elasticsearch
-#cp elasticsearch.yml /tmp/elasticsearch/
-docker run -i -t -P --rm --name elasticsearch -p 9200:9200 -p 9300:9300 -v /tmp/elasticsearch:/data/data bradegler/elasticsearch /elasticsearch/bin/elasticsearch -Des.config=/data/elasticsearch.yml
+
+docker run -i -t -P --rm --name elasticsearch -p 9200:9200 -p 9300:9300 -v /tmp/elasticsearch:/data/data bradegler/elasticsearch /elasticsearch/bin/elasticsearch -Des.config=/elasticsearch/config/elasticsearch.yml
