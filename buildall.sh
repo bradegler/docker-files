8c26754a265738bdd0bd8638ee3448284a3ca815
Brad Egler <begler@gmail.com>
2014-11-03 22:56:28 -0500

    fixed java 7 and 8 to work with debian

--- buildall.sh
+++ buildall.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 docker build -t="bradegler/base" ./base
-docker build -t="bradegler/chef" ./chef
-docker build -t="bradegler/java7" ./java7
-docker build -t="bradegler/jenkins" ./jenkins
-docker build -t="bradegler/rabbitmq" ./rabbitmq
-docker build -t="bradegler/mongodb" ./mongodb
-docker build -t="bradegler/redis" ./redis
-docker build -t="bradegler/node" ./node
+for dir in `ls -d */ | grep -v base | cut -d'/' -f1`;
+do
+    echo "bradegler/$dir"
+    docker build -t="bradegler/$dir" ./$dir
+done;
