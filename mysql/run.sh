656be55301b5c1d2cb2f7a111715cbebb80500c3
Brad Egler <begler@gmail.com>
2014-12-22 13:15:24 -0500

    changed mysql to take root password from the environment

--- mysql/run.sh
+++ mysql/run.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-if [ ! -f /mysql-root-pw.txt ]; then
 
-    /usr/bin/mysqld_safe &
-    sleep 10s
+if [ -z $MYSQL_ROOT_PASSWORD ]; then
 
-    MYSQL_PASSWORD=`pwgen -c -n -1 12`
-
-    echo mysql root password: $MYSQL_PASSWORD
-    echo $MYSQL_PASSWORD > /mysql-root-pw.txt
-
-    mysqladmin -u root password $MYSQL_PASSWORD
-    mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+    MYSQL_ROOT_PASSWORD=`pwgen -c -n -1 12`
 
 fi
+
+/usr/bin/mysqld_safe &
+
+echo Using password $MYSQL_ROOT_PASSWORD
+
+mysqladmin --wait=10 -u root password $MYSQL_ROOT_PASSWORD
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
 supervisord
